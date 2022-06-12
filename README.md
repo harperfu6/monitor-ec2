67d41ab7e03843f120e3bfd223045aef88a8a6c0
harperfu6 <beharp8a8@gmail.com>
2022-06-12 10:05:11 +0900

    add slack notify

---
+++ README.md
@@ -0,0 +1,6 @@
+# monitor-ec2
+EC2の起動状態を監視しRunning状態のインスタンスについてSlackに通知する
+
+```
+$ SLACK_BOT_TOKEN="xoxb-xxxxxx" cargo r 
+```
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
 use aws_sdk_ec2::{model::InstanceStateName, Client, Error};
+use slack::chat::post_message::{post_message, PostMessageRequest};
+use slack::http_client::default_client;
+use slack_rust as slack;
+use std::env;
+use std::fmt::{Display, Write};
 
-async fn show_state(client: &Client, ids: Option<Vec<String>>) -> Result<(), Error> {
+// Monitoring Instance
+#[derive(Debug)]
+struct MInstance {
+    name: String,
+    state: InstanceStateName,
+}
+
+impl Display for MInstance {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} / {:?}", self.name, self.state);
+        f.write_char('\n');
+        Ok(())
+    }
+}
+
+async fn show_state(client: &Client, ids: Option<Vec<String>>) -> Result<Vec<MInstance>, Error> {
     let resp = client
         .describe_instances()
         .set_instance_ids(ids)
         .send()
         .await?;
 
+    let mut m_instance: Vec<MInstance> = Vec::new();
     for reservation in resp.reservations().unwrap_or_default() {
         for instance in reservation.instances().unwrap_or_default() {
             let state = instance.state().unwrap().name().unwrap();
@@ -14,8 +35,15 @@ async fn show_state(client: &Client, ids: Option<Vec<String>>) -> Result<(), Err
                 InstanceStateName::Running => {
                     let tags = instance.tags().unwrap();
                     for t in tags {
-                        if t.key().unwrap() == "Name" {
-                            println!("Name: {:?}", t.value().unwrap());
+                        if let Some(name) = t.key() {
+                            if name == "Name" {
+                                let name_value = t.value().unwrap_or_default().to_string();
+                                println!("Name: {:?}", name_value);
+                                m_instance.push(MInstance {
+                                    name: name_value,
+                                    state: state.clone(),
+                                });
+                            }
                         }
                     }
                     println!("Instance ID: {}", instance.instance_id().unwrap());
@@ -27,16 +55,32 @@ async fn show_state(client: &Client, ids: Option<Vec<String>>) -> Result<(), Err
         }
     }
 
-    Ok(())
+    Ok(m_instance)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // let region_provider = RegionProviderChain::first_try(region.map(Region::new));
     let shared_config = aws_config::load_from_env().await;
     let client = Client::new(&shared_config);
 
-    // let instance_id: Option<Vec<String>> = Some(Vec::from(["i-02ac2100214309b9d".to_string()]));
-    // show_state(&client, instance_id).await
-    show_state(&client, None).await // list all
+    let m_instance_vec: Vec<MInstance> = show_state(&client, None).await?; // list all
+    let message: String = m_instance_vec.iter().map(|mi| mi.to_string()).collect();
+
+    let slack_bot_token =
+        env::var("SLACK_BOT_TOKEN").unwrap_or_else(|_| panic!("slack bot token is not set."));
+
+    let slack_api_client = default_client();
+    let bot_test_channel_id = "C02ML9PTSJD";
+    let param = PostMessageRequest {
+        channel: bot_test_channel_id.to_string(),
+        text: Some(message),
+        ..Default::default()
+    };
+
+    let response = post_message(&slack_api_client, &param, &slack_bot_token)
+        .await
+        .expect("api call error");
+    println!("{:?}", response);
+
+    Ok(())
 }
