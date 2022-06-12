7d04cdac3e118e1dd6f1be549553bff0d8221bde
harperfu6 <beharp8a8@gmail.com>
2022-06-12 10:41:20 +0900

    add webhook version

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # monitor-ec2
 EC2の起動状態を監視しRunning状態のインスタンスについてSlackに通知する
 
+## incomming webhook を使うバージョン
+```
+$ SLACK_CHANNEL_WEBHOOK_URL=https://hooks.slack.com/services/xxxxx cargo r 
+```
+
+## api token を使うバージョン
 ```
 $ SLACK_BOT_TOKEN="xoxb-xxxxxx" cargo r 
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use aws_sdk_ec2::{model::InstanceStateName, Client, Error};
+use curl::easy::Easy;
 use slack::chat::post_message::{post_message, PostMessageRequest};
 use slack::http_client::default_client;
 use slack_rust as slack;
 use std::env;
 use std::fmt::{Display, Write};
+use std::io::Read;
 
 // Monitoring Instance
 #[derive(Debug)]
@@ -58,6 +60,7 @@ async fn show_state(client: &Client, ids: Option<Vec<String>>) -> Result<Vec<MIn
     Ok(m_instance)
 }
 
+// incomming web hook
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let shared_config = aws_config::load_from_env().await;
@@ -66,21 +69,49 @@ async fn main() -> Result<(), Error> {
     let m_instance_vec: Vec<MInstance> = show_state(&client, None).await?; // list all
     let message: String = m_instance_vec.iter().map(|mi| mi.to_string()).collect();
 
-    let slack_bot_token =
-        env::var("SLACK_BOT_TOKEN").unwrap_or_else(|_| panic!("slack bot token is not set."));
+    let message_json_str: String = format!("{{\"text\":\"{}\"}}", message);
+    let mut data = message_json_str.as_bytes();
 
-    let slack_api_client = default_client();
-    let bot_test_channel_id = "C02ML9PTSJD";
-    let param = PostMessageRequest {
-        channel: bot_test_channel_id.to_string(),
-        text: Some(message),
-        ..Default::default()
-    };
+    let webhook_url = env::var("SLACK_CHANNEL_WEBHOOK_URL")
+        .unwrap_or_else(|_| panic!("SLACK_CHANNEL_WEBHOOK_URL is not set."));
+    let mut easy = Easy::new();
+    easy.url(&webhook_url).unwrap();
+    easy.post(true).unwrap();
+    easy.post_field_size(data.len() as u64).unwrap();
 
-    let response = post_message(&slack_api_client, &param, &slack_bot_token)
-        .await
-        .expect("api call error");
-    println!("{:?}", response);
+    let mut transfer = easy.transfer();
+    transfer
+        .read_function(|buf| Ok(data.read(buf).unwrap_or(0)))
+        .unwrap();
+    transfer.perform().unwrap();
 
     Ok(())
 }
+
+// api token
+// #[tokio::main]
+// async fn main() -> Result<(), Error> {
+//     let shared_config = aws_config::load_from_env().await;
+//     let client = Client::new(&shared_config);
+
+//     let m_instance_vec: Vec<MInstance> = show_state(&client, None).await?; // list all
+//     let message: String = m_instance_vec.iter().map(|mi| mi.to_string()).collect();
+
+//     let slack_bot_token =
+//         env::var("SLACK_BOT_TOKEN").unwrap_or_else(|_| panic!("slack bot token is not set."));
+
+//     let slack_api_client = default_client();
+//     let bot_test_channel_id = "C02ML9PTSJD";
+//     let param = PostMessageRequest {
+//         channel: bot_test_channel_id.to_string(),
+//         text: Some(message),
+//         ..Default::default()
+//     };
+
+//     let response = post_message(&slack_api_client, &param, &slack_bot_token)
+//         .await
+//         .expect("api call error");
+//     println!("{:?}", response);
+
+//     Ok(())
+// }
